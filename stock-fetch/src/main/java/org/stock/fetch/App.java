546a0ed126cbb70479758bf75fd7ebf29b949d1a
gyang2007 <gyang2007@foxmail.com>
2014-10-14 14:15:34 +0800

    Stock project init.

---
+++ stock-fetch/src/main/java/org/stock/fetch/App.java
@@ -0,0 +1,13 @@
+package org.stock.fetch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
