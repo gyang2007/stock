51bb0f5b95b1be35ecc93eb34b82fbc2752ae077
gyang2007 <gyang2007@foxmail.com>
2015-01-07 00:26:52 +0800

    Html请求工具类。

---
+++ stock-fetch/src/main/java/org/stock/fetch/util/HttpResponse.java
@@ -0,0 +1,25 @@
+package org.stock.fetch.util;
+
+import java.util.List;
+
+public class HttpResponse {
+	public List<String> contentCollection;
+	public String urlString;
+	public int defaultPort;
+	public String file;
+	public String host;
+	public String path;
+	public int port;
+	public String protocol;
+	public String query;
+	public String ref;
+	public String userInfo;
+	public String content;
+	public String contentEncoding;
+	public int code;
+	public String message;
+	public String contentType;
+	public String method;
+	public int connectTimeout;
+	public int readTimeout;
+}
